9fafa24cd3f4bfa7097fb6ff653bc8a50249233d
Tushar Goswami <tushargoswami6263@gmail.com>
2026-09-21 22:32:57 +0530

    okfortheday

--- .netlify/functions-internal/server/_ssr/ssr.mjs
+++ .netlify/functions-internal/server/_ssr/ssr.mjs
@@ -93,7 +93,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-DHxek-rL.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-Dotxi4AM.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
